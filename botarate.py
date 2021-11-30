d9488c0c1ffafa1b354f05e49c5569277ef19231
pablomelado <92090562+pablomelado@users.noreply.github.com>
2021-11-30 14:19:00 +0100

    Update botarate.py
    INFORME CONVERSACIÓN TERMINADO

--- botarate.py
+++ botarate.py
@@ -100,6 +100,46 @@ preguntas_respuestas = {"¿Que equipo de futbol juega hoy?": "Hoy juega el Betis
                         "¿Dime un número aleatorio del 1 al 10?": "Su numero aleatorio es: 929293",
                         "Hola, soy Alex": "Muy buenas, Alex. Soy Botarate",
                         "¿Quien te ha creado?": "Me ha creado el grupo 3"}
+
+def botInformeConver():
+    from reportlab.pdfgen import canvas
+    from reportlab.lib.pagesizes import A4
+    from datetime import date
+    from collections import defaultdict
+
+    today = date.today()  # Fecha del sistema en el momento de la conversión a pdf ¿¿Fecha de conversación original??
+
+    conversacion = open('conversación.txt', "r", encoding="utf-8")
+    lines = conversacion.read()
+    c = canvas.Canvas("conversación.pdf", pagesize=A4)
+    y = 500
+
+    for line in lines.split('\n'):
+        c.drawString(50, y, line)
+        y = y - 25
+
+
+    numcar = len(lines)  # Conteo de carácteres
+    listpal = lines.split()  # Split de la cadena
+    numpal = len(listpal)  # Conteo de palabras en cadena
+
+    temp = defaultdict(int)
+
+    for sub in listpal:
+        for wrd in sub.split():
+            temp[wrd] += 1
+
+    palrep = max(temp, key=temp.get)
+    numrep = listpal.count(palrep)
+
+    c.drawImage("chatbot_python.jpg", 100, 600, width=400, height=200)  # Imagen del directorio
+    c.drawString(100, 575, 'INFORME DE LA CONVERSACIÓN')
+    c.drawString(50, 100, 'La conversación es del '+str(today)+'.')  # CAMBIAR A FECHA DE LA CONVERSACIÓN DESDE TXT
+    c.drawString(50, 75, 'Consta de '+str(numcar)+' caracteres.')
+    c.drawString(50, 50, 'Está compuesta por '+str(numpal)+' palabras.')
+    c.drawString(50, 25, 'La palabra '+str(palrep)+' se repite '+str(numrep)+' veces.')
+    c.save()
+
 salir = False
 opcion = 0
 
@@ -119,7 +159,7 @@ while not salir:
     elif opcion == 3:
         print("\t\t\t3) Bot simple mejorado desde fichero")
     elif opcion == 4:
-        print("\t\t\t4) Informe de la conversación (PDF)")
+        botInformeConver()
     elif opcion == 5:
         print("\t\t\t5) Salir\n")
         salir = True
